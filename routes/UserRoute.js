daed91df4c70d67fc9c0427533f7525bed65cbe0
Tasheela Jayawickrama <tasheelajay1999@gmail.com>
2025-10-04 17:04:14 +0530

    users done

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const {register, login, updateUser, getAllStudents} = require('../controllers/UserController');
+const {register, login, updateUser, getAllUsers, getByUserRole, getUserById} = require('../controllers/UserController');
+
 
 
 router.post('/register', register);
 router.post('/login', login);
 router.put('/update/:id', updateUser);
+router.get('/', getAllUsers);
+router.get('/role/:role', getByUserRole);
+router.get('/:id', getUserById);
 
 
 module.exports = router;
